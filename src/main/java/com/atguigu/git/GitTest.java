8352fe767b34a2049c7a6d2a5debe35594944969
paipai <3218136307@qq.com>
2023-08-17 14:40:21 +0800

    first commit

---
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -0,0 +1,11 @@
+package com.atguigu.git;
+
+/**
+ * @author 李兵
+ * @create 2023-08-17 14:38
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("hello git!");
+    }
+}
